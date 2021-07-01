b3f98ec0fe4327f7ded32173af34f49748c227aa
ronip <ronipakter@gmail.com>
2021-07-01 19:27:52 +0300

    store component

--- client/src/components/store/store.js
+++ client/src/components/store/store.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import { useSelector } from 'react-redux';
 import Prodect from './prodect/prodect';
 import useStyles from './styles';
@@ -9,19 +22,68 @@ const Store= ({setCurrentId})=>{
     const Store = useSelector((state) => state.Store);
     const classes = useStyles();
 
+    
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     console.log(Store);
-    return (
-     /*   !Store.length ? <CircularProgress /> : (
-            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {Store.map((Prodect) => (
-                    <Grid key={Prodect._id} item xs={12} sm={6}>
-                        <Prodect post={Prodect} setCurrentId={setCurrentId} />
-                    </Grid>
-                ))}
-            </Grid>*/
-            <h1>i am in store</h1>
+    return (     
+        <React.Fragment>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <CameraIcon className={classes.icon} />
+            <Typography variant="h6" color="inherit" noWrap>
+              Album layout
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <Container className={classes.cardGrid} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://source.unsplash.com/random"
+                      title="Image title"
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. You can use this section to describe the content.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                      <Button size="small" color="primary">
+                        Edit
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <footer className={classes.footer}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            Something here to give the footer a purpose!
+          </Typography>
+        </footer>
+        {/* End footer */}
+      </React.Fragment>
         )
- //   );
 }
 
 export default Store;
